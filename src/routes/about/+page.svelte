a2566cebcd565a96537f44b2ee0722088a593cb8
Adwithya Magow <avmagow@gmail.com>
2022-11-16 16:30:41 +0800

    Chagne about page to talk about myself

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,6 @@
-<section class="text-center w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300">
-    <h1 class="pt-10 text-7xl">About page</h1>
-    <div class="font-serif pt-12 text-2xl">Ciao!</div>
-    </section>
+<section
+	class="text-center w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300"
+>
+	<h1 class="pt-10 text-7xl">Hello, I am Adwithya Magow</h1>
+	<h2 class="font-serif pt-12 text-2xl">I'm a full-stack developer</h2>
+</section>
