bf333360c0be0660d869cc4c129005f4766c758d
Adwithya Magow <avmagow@gmail.com>
2022-11-18 22:31:52 +0800

    Make text center of screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <section
-	class="text-center w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300"
+	class="flex flex-col justify-center 
+		bg-gradient-to-r 
+		h-screen w-screen 
+		from-yellow-200 via-red-300 to-pink-300"
 >
-	<h1 class="pt-10 text-7xl">Hello, I am Adwithya Magow</h1>
+	<h1 class="pt-10 text-7xl">Hey, I am <br /> Adwithya Magow</h1>
 	<h2 class="font-serif pt-12 text-2xl">I'm a full-stack developer</h2>
 </section>
