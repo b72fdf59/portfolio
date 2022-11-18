0911efa4000342a4f70640c644984fff36ecbafe
Adwithya Magow <avmagow@gmail.com>
2022-11-18 22:34:03 +0800

    Add padding to the left

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
 		h-screen w-screen 
 		from-yellow-200 via-red-300 to-pink-300"
 >
-	<h1 class="pt-10 text-7xl">Hey, I am <br /> Adwithya Magow</h1>
-	<h2 class="font-serif pt-12 text-3xl">I'm a full-stack developer</h2>
+	<div class="pl-10">
+		<h1 class="pt-10 text-7xl">Hey, I am <br /> Adwithya Magow</h1>
+		<h2 class="font-serif pt-12 text-3xl">I'm a full-stack developer</h2>
+	</div>
 </section>
