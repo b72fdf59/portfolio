16a6f425e7db3a36daeaaba5b749047cb721e396
Adwithya Magow <avmagow@gmail.com>
2022-11-18 22:32:16 +0800

    Make h2 size a bit bigger

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
 		from-yellow-200 via-red-300 to-pink-300"
 >
 	<h1 class="pt-10 text-7xl">Hey, I am <br /> Adwithya Magow</h1>
-	<h2 class="font-serif pt-12 text-2xl">I'm a full-stack developer</h2>
+	<h2 class="font-serif pt-12 text-3xl">I'm a full-stack developer</h2>
 </section>
