fd779a8de8d0c1c4c42e63fa218547f039c9b4e9
Adwithya Magow <avmagow@gmail.com>
2022-11-18 15:07:40 +0800

    Configure for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,13 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: null
-		})
+		}),
+		paths: {
+			base: "/portfolio"
+		}
 	}
 };
 
