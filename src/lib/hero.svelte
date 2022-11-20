a8749448f33f6a6414ddcd2fa7b8c34195ce553c
Adwithya Magow <avmagow@gmail.com>
2022-11-21 00:35:50 +0800

    add bottom chevron for hero page

--- src/lib/hero.svelte
+++ src/lib/hero.svelte
@@ -9,6 +9,25 @@
 		<h1 class="pt-10 text-7xl text-white">Hey, I am <br /> Adwithya Magow</h1>
 		<h2 class="font-serif pt-12 text-3xl text-white">I'm a full-stack developer</h2>
 	</div>
+	<div >
+		<button class="absolute bottom-10 text-white rotate-180">
+			<svg
+				width="80"
+				height="80"
+				viewBox="0 0 80 80"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M16.0371 52.3672L36.8551 31.5492C38.6125 29.7918 41.4617 29.7918 43.2191 31.5492L64.0371 52.3672"
+					stroke="#FFFFFF"
+					stroke-width="6"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			</svg>
+		</button>
+	</div>
 </section>
 
 <style>
