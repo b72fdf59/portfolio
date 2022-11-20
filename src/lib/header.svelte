7ddcb4442178171b3868b585c00a9b623c355fb1
Adwithya Magow <avmagow@gmail.com>
2022-11-20 23:44:44 +0800

    Add Header component

---
+++ src/lib/header.svelte
@@ -0,0 +1,17 @@
+<header>
+	<nav class="w-full flex items-center justify-between relative py-2 bg-white shadow-md">
+		<div>Adwithya Magow</div>
+		<div class="navbar-collapse collapse grow items-center">
+			<ul class="navbar-nav mr-auto lg:flex lg:flex-row">
+				<li class="nav-item">
+					<a
+						class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+						href="#!"
+					>
+						Home
+					</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</header>
