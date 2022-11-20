da0e9fb972831b3db0995a6f4ac173d58561cd51
Adwithya Magow <avmagow@gmail.com>
2022-11-20 15:24:30 +0800

    Add custom gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,30 @@
 	class="flex flex-col justify-center 
 		bg-gradient-to-r 
 		h-screen w-screen 
-		from-yellow-200 via-red-300 to-pink-300"
+		custom-gradient"
 >
 	<div class="pl-10">
-		<h1 class="pt-10 text-7xl">Hey, I am <br /> Adwithya Magow</h1>
-		<h2 class="font-serif pt-12 text-3xl">I'm a full-stack developer</h2>
+		<h1 class="pt-10 text-7xl text-white">Hey, I am <br /> Adwithya Magow</h1>
+		<h2 class="font-serif pt-12 text-3xl text-white">I'm a full-stack developer</h2>
 	</div>
 </section>
+
+<style>
+	.custom-gradient {
+		background-image: linear-gradient(
+			to right top,
+			#d16ba5,
+			#c777b9,
+			#ba83ca,
+			#aa8fd8,
+			#9a9ae1,
+			#8aa7ec,
+			#79b3f4,
+			#69bff8,
+			#52cffe,
+			#41dfff,
+			#46eefa,
+			#5ffbf1
+		);
+	}
+</style>
