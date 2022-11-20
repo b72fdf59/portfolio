e2384fe31e24d28feb33d6024551b78b0616f492
Adwithya Magow <avmagow@gmail.com>
2022-11-21 00:11:14 +0800

    Create a basic header component

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,12 +1,14 @@
 <header>
-	<nav class="w-full flex items-center justify-between relative py-2 bg-white shadow-md">
+	<nav
+		class="h-[5vh] w-full flex items-center justify-between relative py-2 px-2 bg-white shadow-md"
+	>
 		<div>Adwithya Magow</div>
-		<div class="navbar-collapse collapse grow items-center">
-			<ul class="navbar-nav mr-auto lg:flex lg:flex-row">
-				<li class="nav-item">
+		<div class="flex items-center">
+			<ul>
+				<li>
 					<a
-						class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
-						href="#!"
+						class="nav-link block text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+						href="#landing"
 					>
 						Home
 					</a>
