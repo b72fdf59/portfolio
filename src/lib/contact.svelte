40d76a43d8b5c930c9f44fd0687f2a6a5b239b1e
Adwithya Magow <avmagow@gmail.com>
2022-11-21 15:29:58 +0800

    Make some styling on the contact section

--- src/lib/contact.svelte
+++ src/lib/contact.svelte
@@ -1,7 +1,44 @@
-<section
-    id="contact"
-	class="flex items-center justify-center
-            h-screen"
->
-	<h1>Contact</h1>
+<section id="contact" class="flex flex-col items-center justify-center h-[66.67vh]">
+	<h1 class="section-header relative text-center text-5xl mb-40">Contact</h1>
+	<div class="flex flex-col block min-h-[25vh] w-5/6 max-w-2xl py-5 px-24 rounded-lg shadow-lg custom-gradient">
+		<h2 class="text-center text-4xl mb-14">Get In Touch</h2>
+		<div class="flex flex-col items-center">
+			<p class="text-center mb-10">
+				I am always looking for new avenues to learn and grow. Feel free to reach out whether you
+				would like to say hi, ask a qustion or would like to share an opportunity. I will get back
+				to you!
+			</p>
+			<button
+				class="inline-block px-6 py-2.5 
+						bg-[#86A8E7] text-white 
+						font-medium text-xs leading-tight 
+						uppercase rounded 
+						hover:bg-[#D16BA5]
+						focus:bg-[#D16BA5] focus:outline-none focus:ring-0 
+						active:bg-[#952E69]
+						transition duration-150 ease-in-out"
+			>
+				<a
+					class="email-link"
+					href="mailto:avmagow@gmail.com"
+					rel="noopener noreferrer"
+					target="_blank">Contact Now</a
+				>
+			</button>
+		</div>
+	</div>
 </section>
+
+<style>
+	.container {
+		background-attachment: fixed;
+	}
+	.container:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+} 
+</style>
